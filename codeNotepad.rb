799db56cacc17b28211df4e05973efcfa312869d
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-03-14 20:25:34 +0000

    deleted codenotepad

--- codeNotepad.rb
+++
@@ -1,9 +0,0 @@
-require './fight'
-
-player = {:Health => 5000, :Strength => 100, :Luck => 1.1, :Level => 1, :Exp => 0, :Gold => 0}
-monster = {:Health => 1000, :Strength => 75, :Luck => 0.5, :Level => 1}
-
-fight(player, monster)
-puts "Your health: #{player[:Health]}"
-puts "Exp gained: #{player[:Exp]}"
-puts "Gold found: #{player[:Gold]}"
